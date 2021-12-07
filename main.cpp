2fd919e201d63eb1b37c2850d9a3df654cd36ddb
Twinkle264 <twinkle264@gmail.com>
2021-12-07 17:46:28 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    float matrixM[4][4];
+    vector<float> vec(4);
+    vector<float> result(4);
+
+    cout << "Input the matrix:" << endl;
+    for (int i = 0; i < 4; ++i){
+        for (int j = 0; j < 4; ++j) {
+            cin >> matrixM[i][j];
+        }
+    }
+
+    cout << endl << "Input the vector:" << endl;
+    for (int i = 0; i < vec.size(); ++i) {
+        cin >> vec[i];
+    }
+
+    cout << endl << "Matrix:" << endl;
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            cout << matrixM[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    cout << endl << "Vector:" << endl;
+
+    for (int i = 0; i < 4; ++i) {
+        cout << vec[i] << ' ';
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result[i] += vec[i] * matrixM[j][i];
+        }
+    }
+
+    cout << endl << "Result:" << endl;
+    for (int i = 0; i < 4; ++i) {
+        cout << result[i] << ' ';
+    }
+
+    return 0;
+}
